032bbee34d0d08aa636740e0679cca55a8f8d2c0
Santosh Kumar Singh <santosh@gametarian.com>
2018-03-21 12:23:33 +0530

    Source code first push commit

---
+++ MyGitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGitTest
+//
+//  Created by Prabal Mukherjee on 21/3/18.
+//  Copyright © 2018 Prabal Mukherjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyGitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyGitTest
+//
+//  Created by Prabal Mukherjee on 21/3/18.
+//  Copyright © 2018 Prabal Mukherjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
